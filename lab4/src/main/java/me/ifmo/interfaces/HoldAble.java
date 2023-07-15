d3deb2305b25dcd851528b313eaf3e50fa3016df
misticslippers <eternal.martyr13579@gmail.com>
2023-07-15 23:06:38 +0300

    lab3

---
+++ lab4/src/main/java/me/ifmo/interfaces/HoldAble.java
@@ -0,0 +1,6 @@
+package me.ifmo.interfaces;
+
+@FunctionalInterface
+public interface HoldAble{
+    String hold();
+}
---
+++ lab4/src/main/java/me/ifmo/interfaces/StayAble.java
@@ -0,0 +1,6 @@
+package me.ifmo.interfaces;
+
+@FunctionalInterface
+public interface StayAble{
+    String stay();
+}
